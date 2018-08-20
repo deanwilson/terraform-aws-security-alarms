f3bf0a48061ed28ffe93e98ea6f5537445d97129
Dean Wilson <dean.wilson@gmail.com>
2018-08-20 16:45:34 +0100

    Add a basic usage example

---
+++ examples/basic-alarms/main.tf
@@ -0,0 +1,26 @@
+# use an external module to configure cloud trail
+# you will also need to configure your own action endpoint
+
+module "cloudtrail" {
+  source    = "deanwilson/cloudtrail"
+  namespace = "unixdaemon"
+}
+
+# add the console signing alarm
+module "console-signin-failure-alarm" {
+  source    = "aws-security-alarms/modules/console-signin-failure"
+  namespace = "unixdaemon"
+
+  cloudtrail_log_group_name = "${module.cloudtrail.cloudtrail_log_group_name}"
+  alarm_actions             = ["${module.admin-sns-email-topic.arn}"]
+}
+
+# add the root account activity alarm. Note most of the
+# code is boilerplate
+module "root-account-activity-alarm" {
+  source    = "aws-security-alarms/modules/root-account-activity"
+  namespace = "unixdaemon"
+
+  cloudtrail_log_group_name = "${module.cloudtrail.cloudtrail_log_group_name}"
+  alarm_actions             = ["${module.admin-sns-email-topic.arn}"]
+}
